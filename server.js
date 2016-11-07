270f5d9b18e01739b64df1e2146bfec83bbfb26a
KSAkhil <sivamaniakhil009@gmail.com>
2016-11-07 20:04:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/Capture.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Capture.png'));
+app.get('/ui/Capture.PNG', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Capture.PNG'));
 });
 
 
